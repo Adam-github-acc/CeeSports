92f9b054828614c6eafb6cb091bfe50d98486317
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-11-10 16:39:20 +0100

    fix(node):increase payload limit bc of post

--- server/index.ts
+++ server/index.ts
@@ -6,8 +6,8 @@ const app: Application = express();
 const prisma = new PrismaClient();
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: true}));
 
 const PORT: number | string = process.env.PORT || 3000;
 
